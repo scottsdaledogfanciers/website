b8d339d29e4adfc3cefe12d922563969037cd78b
Jared Reisinger <jaredreisinger@hotmail.com>
2022-10-20 20:10:59 -0700

    update favicon and README

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,12 @@ const { DateTime } = require('luxon');
 const htmlmin = require('html-minifier');
 const Image = require('@11ty/eleventy-img');
 
-async function imageShortcode(src, alt, sizes, options={}) {
+async function imageShortcode(src, alt, sizes, options = {}) {
   let metadata = await Image(src, {
     widths: [null, 1280, 1024, 600],
-    formats: ["webp", "jpg"],
+    formats: ['webp', 'jpg'],
     outputDir: './_site/static/img/',
-    urlPath: '/static/img/'
+    urlPath: '/static/img/',
   });
 
   // console.log("\n\nMETADATA:", metadata, "\n\n");
@@ -17,8 +17,8 @@ async function imageShortcode(src, alt, sizes, options={}) {
   const attributes = {
     alt,
     sizes,
-    loading: "lazy",
-    decoding: "async",
+    loading: 'lazy',
+    decoding: 'async',
   };
 
   // console.log("\n\nIMAGE INFO:", imageAttributes, "\n\n");
@@ -34,19 +34,23 @@ async function imageShortcode(src, alt, sizes, options={}) {
 
 // lifted from 11ty/image
 function mapObjectToHTML(tagName, attrs = {}) {
-  let attrHtml = Object.entries(attrs).map(entry => {
-    let [key, value] = entry;
-    // hack: remove width/height on img to allow 100% width...
-    if (key === 'width' || key === 'height') { return ''; }
-    return `${key}="${value}"`;
-  }).join(" ");
+  let attrHtml = Object.entries(attrs)
+    .map((entry) => {
+      let [key, value] = entry;
+      // hack: remove width/height on img to allow 100% width...
+      if (key === 'width' || key === 'height') {
+        return '';
+      }
+      return `${key}="${value}"`;
+    })
+    .join(' ');
 
-  return `<${tagName}${attrHtml ? ` ${attrHtml}` : ""}>`;
+  return `<${tagName}${attrHtml ? ` ${attrHtml}` : ''}>`;
 }
 
 // The exported Image.generateHTML does not provide a means to tweak the
 // elements much... this adds that.
-function generateBetterHTML(metadata, attributes, options={}) {
+function generateBetterHTML(metadata, attributes, options = {}) {
   // let isInline = options.whitespaceMode !== "block";
   const isInline = true;
   let markup = [];
@@ -55,42 +59,51 @@ function generateBetterHTML(metadata, attributes, options={}) {
 
   // obj is either { img: {...} }, or { picture: [...] }; we apply top-level
   // options either way.
-  for(let tag in obj) {
-    if(!Array.isArray(obj[tag])) {
+  for (let tag in obj) {
+    if (!Array.isArray(obj[tag])) {
       const attrs = { ...obj[tag] };
       if (options?.class) {
-        attrs.class = `${attrs.class}${attrs.class ? ' ' : ''}${options.class}`
+        attrs.class = `${attrs.class}${attrs.class ? ' ' : ''}${options.class}`;
       }
       if (options?.style) {
-        attrs.style = `${attrs.style}${attrs.style ? ' ' : ''}${options.style}`
+        attrs.style = `${attrs.style}${attrs.style ? ' ' : ''}${options.style}`;
       }
       markup.push(mapObjectToHTML(tag, obj[tag]));
     } else {
-      markup.push(`<${tag}${options?.class ? ` class="${options.class}"` : ''}${options?.style ? ` style="${options.style}"` : ''}>`);
-      for(let child of obj[tag]) {
+      markup.push(
+        `<${tag}${options?.class ? ` class="${options.class}"` : ''}${
+          options?.style ? ` style="${options.style}"` : ''
+        }>`
+      );
+      for (let child of obj[tag]) {
         let childTagName = Object.keys(child)[0];
-        markup.push((!isInline ? "  " : "") + mapObjectToHTML(childTagName, child[childTagName]));
+        markup.push(
+          (!isInline ? '  ' : '') +
+            mapObjectToHTML(childTagName, child[childTagName])
+        );
       }
       markup.push(`</${tag}>`);
     }
   }
-  return markup.join(!isInline ? "\n" : "");
+  return markup.join(!isInline ? '\n' : '');
 }
 
-async function backgroundImageShortcode(src, quote="'") {
+async function backgroundImageShortcode(src, quote = "'") {
   let metadata = await Image(src, {
     widths: [1280],
-    formats: ["webp", "jpeg"],
+    formats: ['webp', 'jpeg'],
     outputDir: './_site/static/img/',
-    urlPath: '/static/img/'
+    urlPath: '/static/img/',
   });
 
   const formats = Object.keys(metadata);
 
-  const imageSet = formats.map(format => {
-    const url = metadata[format][0].url
-    return `url(${quote}${url}${quote}) type(${quote}image/${format}${quote})`
-  }).join(", ");
+  const imageSet = formats
+    .map((format) => {
+      const url = metadata[format][0].url;
+      return `url(${quote}${url}${quote}) type(${quote}image/${format}${quote})`;
+    })
+    .join(', ');
 
   // Many browsers don't support the "by type", so we include non-image-set as
   // well...
@@ -100,9 +113,8 @@ async function backgroundImageShortcode(src, quote="'") {
     `image-set(${imageSet})`,
   ];
 
-  return bgImageKinds.map(kind => `background-image: ${kind};`).join(" ");
+  return bgImageKinds.map((kind) => `background-image: ${kind};`).join(' ');
 }
-
 
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
@@ -118,43 +130,55 @@ module.exports = function (eleventyConfig) {
     );
   });
 
-  eleventyConfig.addShortcode('year', () => new Date().getFullYear().toString());
-
-  // eleventyConfig.addNunjucksGlobal('year', new Date().getFullYear().toString());
+  eleventyConfig.addShortcode('year', () =>
+    new Date().getFullYear().toString()
+  );
 
   // Allow YAML everywhere that JSON is supported.
   eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
 
   // Set up image processing...
-  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
-  eleventyConfig.addLiquidShortcode("image", imageShortcode);
-  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
+  eleventyConfig.addLiquidShortcode('image', imageShortcode);
+  eleventyConfig.addJavaScriptFunction('image', imageShortcode);
 
-  eleventyConfig.addNunjucksAsyncShortcode("backgroundImage", backgroundImageShortcode);
-  eleventyConfig.addLiquidShortcode("backgroundImage", backgroundImageShortcode);
-  eleventyConfig.addJavaScriptFunction("backgroundImage", backgroundImageShortcode);
-
-  // Copy Static Files to /_Site
-
-  eleventyConfig.addPassthroughCopy({
-    './node_modules/alpinejs/dist/cdn.min.js': './static/js/alpine.js',
-    './node_modules/prismjs/themes/prism-tomorrow.css':
-      './static/css/prism-tomorrow.css',
-  });
-
-  // Netlify CMS gets transformed for local vs. production...
-  const cmsConfig = `./src/admin/config${(process.env.NODE_ENV || 'development') === 'development' ? '.dev' : ''}.yml`;
-  eleventyConfig.addPassthroughCopy(
-    {
-      [cmsConfig]: './admin/config.yml',
-    }
+  eleventyConfig.addNunjucksAsyncShortcode(
+    'backgroundImage',
+    backgroundImageShortcode
+  );
+  eleventyConfig.addLiquidShortcode(
+    'backgroundImage',
+    backgroundImageShortcode
+  );
+  eleventyConfig.addJavaScriptFunction(
+    'backgroundImage',
+    backgroundImageShortcode
   );
 
-  // Copy Image Folder to /_site
-  eleventyConfig.addPassthroughCopy('./src/static/media');
+  // Netlify CMS gets transformed for local vs. production...
+  const cmsConfig = `./src/admin/config${
+    (process.env.NODE_ENV || 'development') === 'development' ? '.dev' : ''
+  }.yml`;
+  eleventyConfig.addPassthroughCopy({
+    [cmsConfig]: './admin/config.yml',
+  });
+
+  // copy dependency files to /_site
+  eleventyConfig.addPassthroughCopy({
+    'node_modules/alpinejs/dist/cdn.min.js': 'static/js/alpine.js',
+  });
+
+  // copy media folder to /_site
+  eleventyConfig.addPassthroughCopy('src/static/media');
+
+  // copy favicon folder to /_site (and special copy for '/favicon.ico')
+  eleventyConfig.addPassthroughCopy({
+    'src/static/favicon/favicon.ico': 'favicon.ico',
+  });
+    eleventyConfig.addPassthroughCopy('src/static/favicon');
 
   // Copy favicon to route of /_site
-  eleventyConfig.addPassthroughCopy('./src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('src/favicon.ico');
 
   // Minify HTML
   eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# Scottsdale Dog Fanciers Association Website
+
+<table style="border: none; border-bottom: 1px solid #cccccc">
+  <tr style="border: none">
+    <td style="border: none"><img src="./src/static/media/logo-150.png" width="100"></td>
+    <td style="border: none">
+      <h1 style="border-bottom: none">Scottsdale Dog Fanciers Association Website</h1>
+    </td>
+  </tr>
+</table>
+
+[![netlify status](https://img.shields.io/netlify/a19862c5-af15-48ae-b8ce-19340cbfd844?logo=netlify)](https://app.netlify.com/sites/scottsdaledogfanciersassn-com/deploys)
+
 
 What will eventually become the official source for https://scottsdaledogfanciersassn.com; a NEAT (**N**etlify CMS, **E**leventy, **A**lpine JS & **T**ailwind CSS) website.
 
